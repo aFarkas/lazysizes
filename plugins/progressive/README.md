30f65f801736d9b63b28eada98e222f4ef8c2637
Jakob Schröter <jakob.schroeter@rukzuk.com>
2015-01-19 15:48:48 +0100

    first version of plugin to support progressive rendering of JPG/PNGs when using LQIP pattern

---
+++ plugins/progressive/README.md
@@ -0,0 +1,5 @@
+#lazysizes progressive extension
+
+This lazySizes extension adds better support for browser rendering of progressive jpgs/pngs.
+Needs a proper low quality src, so use it with the LQIP pattern.
+When the lazysizes detects the image gets visible, the src will be inserted as background image until the image from srcset is completely loaded.
---
+++ plugins/progressive/ls.progressive.js
@@ -0,0 +1,34 @@
+/*
+This lazySizes extension adds better support for browser rendering of progressive jpgs/pngs.
+Needs a proper low quality src, so use it with the LQIP pattern. 
+When the lazysizes detects the image gets visible, the src will be inserted as background image until the image from srcset is completely loaded.
+*/
+(function(document){
+	/*jshint eqnull:true */
+	'use strict';
+	var regImg, onload;
+
+	if(document.addEventListener){
+        regImg = /^img$/i;
+
+        onload = function(e){
+            e.target.style.backgroundSize = '';
+            e.target.style.backgroundImage = '';
+            e.target.removeEventListener(e.type, onload);
+        };
+
+        document.addEventListener('lazybeforeunveil', function(e){
+            var img = e.target;
+            if(!regImg.test(img.nodeName)){
+                return;
+            }
+            var src = img.getAttribute('src');
+            if(src) {
+                img.style.backgroundSize = 'cover';
+                img.style.backgroundImage = 'url(' + src + ')';
+                img.removeAttribute('src');
+                img.addEventListener('load', onload);
+            }
+        }, false);
+	}
+})(document);
