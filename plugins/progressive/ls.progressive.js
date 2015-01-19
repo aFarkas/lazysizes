d1a0ef47b5e22c0d7a74bf892da5cb7ae5021933
Jakob Schröter <jakob.schroeter@rukzuk.com>
2015-01-19 18:01:55 +0100

    fixed whitespaces

--- plugins/progressive/ls.progressive.js
+++ plugins/progressive/ls.progressive.js
@@ -9,26 +9,26 @@ When the lazysizes detects the image gets visible, the src will be inserted as b
 	var regImg, onLoad;
 
 	if(document.addEventListener){
-        regImg = /^img$/i;
+		regImg = /^img$/i;
 
-        onLoad = function(e){
-            e.target.style.backgroundSize = '';
-            e.target.style.backgroundImage = '';
-            e.target.removeEventListener(e.type, onLoad);
-        };
+		onLoad = function(e){
+			e.target.style.backgroundSize = '';
+			e.target.style.backgroundImage = '';
+			e.target.removeEventListener(e.type, onLoad);
+		};
 
-        document.addEventListener('lazybeforeunveil', function(e){
-            var img = e.target;
-            if(!regImg.test(img.nodeName)){
-                return;
-            }
-            var src = img.getAttribute('src');
-            if(src) {
-                img.style.backgroundSize = '100% 100%';
-                img.style.backgroundImage = 'url(' + src + ')';
-                img.removeAttribute('src');
-                img.addEventListener('load', onLoad);
-            }
-        }, false);
+		document.addEventListener('lazybeforeunveil', function(e){
+			var img = e.target;
+			if(!regImg.test(img.nodeName)){
+				return;
+			}
+			var src = img.getAttribute('src');
+			if(src) {
+				img.style.backgroundSize = '100% 100%';
+				img.style.backgroundImage = 'url(' + src + ')';
+				img.removeAttribute('src');
+				img.addEventListener('load', onLoad);
+			}
+		}, false);
 	}
 })(document);
