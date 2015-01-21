0f1b6a290bb03a6a6c7d2364c8a22f8df23fd741
Jakob Schröter <jakob.schroeter@rukzuk.com>
2015-01-21 13:12:13 +0100

    docs

--- plugins/progressive/ls.progressive.js
+++ plugins/progressive/ls.progressive.js
@@ -1,14 +1,12 @@
 /*
 This lazySizes extension adds better support for browser rendering of progressive jpgs/pngs.
-Needs a usable low quality src, so use it with the LQIP pattern.
-When the lazysizes detects the image gets visible, the src will be inserted as background image until the image from srcset is completely loaded.
 */
 (function(document){
 	/*jshint eqnull:true */
 	'use strict';
 	var regImg, onLoad;
 
-	if(document.addEventListener && 'srcset' in document.createElement('img')){
+	if('srcset' in document.createElement('img')){
 		regImg = /^img$/i;
 
 		onLoad = function(e){
