34552b893181b722a8c86184ed4efca6579725ce
aFarkas <info@corrupt-system.de>
2015-05-12 21:42:32 +0200

    improve ff picture bug fix

--- plugins/static-gecko-picture/ls.static-gecko-picture.js
+++ plugins/static-gecko-picture/ls.static-gecko-picture.js
@@ -5,7 +5,7 @@
 	/*jshint eqnull:true */
 	var ua = navigator.userAgent;
 
-	if(window.HTMLPictureElement && ((/ecko/).test(ua) && ua.match(/rv\:(\d+)/) && RegExp.$1 < 40)){
+	if(window.HTMLPictureElement && ((/ecko/).test(ua) && ua.match(/rv\:(\d+)/) && RegExp.$1 < 41)){
 		addEventListener('resize', (function(){
 			var timer;
 
