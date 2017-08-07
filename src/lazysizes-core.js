83d91bcf4a7b966e571a9b0fc7798572ea50f492
Peter V <petvas@users.noreply.github.com>
2017-08-07 09:52:09 -0400

    Update lazysizes-core.js
    `naturalWidth` typo and backwards compatible

--- src/lazysizes-core.js
+++ src/lazysizes-core.js
@@ -361,11 +361,10 @@ function l(window, document) {
 		var throttledCheckElements = throttle(checkElements);
 
 		var switchLoadingClass = function(e){
-			if(regImg.test(e.nodeName) && !e.naturalwidth){
+			if(regImg.test(e.nodeName) && !e.naturalWidth){
 				addClass(e.target, lazySizesConfig.errorClass);
-			} else {
-				addClass(e.target, lazySizesConfig.loadedClass);
 			}
+			addClass(e.target, lazySizesConfig.loadedClass);
 			removeClass(e.target, lazySizesConfig.loadingClass);
 			addRemoveLoadEvents(e.target, rafSwitchLoadingClass);
 			triggerEvent(e.target, 'lazyloaded');
