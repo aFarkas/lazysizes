107be77ee6cd505038f2577ca53e99d486c76b99
Alexander Farkas <alex@boffinhouse.com>
2019-08-04 19:43:13 +0200

    Make yt thumb size configurable (see #681)

--- plugins/video-embed/ls.video-embed.js
+++ plugins/video-embed/ls.video-embed.js
@@ -25,8 +25,9 @@
 	;
 	var idIndex = Date.now();
 	var regId = /\{\{id}}/;
+	var regYtImg = /\{\{hqdefault}}/;
 	var regAmp = /^&/;
-	var youtubeImg = protocol + '//img.youtube.com/vi/{{id}}/hqdefault.jpg';
+	var youtubeImg = protocol + '//img.youtube.com/vi/{{id}}/{{hqdefault}}.jpg';
 	var youtubeIframe = protocol + '//www.youtube.com/embed/{{id}}?autoplay=1';
 	var vimeoApi = protocol + '//vimeo.com/api/oembed.json?url=https%3A//vimeo.com/{{id}}';
 	var vimeoIframe = protocol + '//player.vimeo.com/video/{{id}}?autoplay=1';
@@ -77,7 +78,9 @@
 	}
 
 	function embedYoutubeImg(id, elem){
-		elem.style.backgroundImage = 'url('+ youtubeImg.replace(regId, id) +')';
+		var ytImg = elem.getAttribute('data-thumb-size') || lazySizes.cfg.ytThumb || 'hqdefault';
+
+		elem.style.backgroundImage = 'url('+ youtubeImg.replace(regId, id).replace(regYtImg, ytImg) +')';
 		elem.addEventListener('click', embedYoutubeIframe);
 	}
 
