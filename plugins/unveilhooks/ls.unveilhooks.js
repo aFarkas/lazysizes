313d427b0441ec3c83808d648cd6ef1b0b97d2dd
js-shane <js-shane@tl.com>
2019-10-02 20:36:19 +0700

    avoid flicker when we change css property background-image if your image load under these circumstances: cache disabled, 304 response, 302 redirect

--- plugins/unveilhooks/ls.unveilhooks.js
+++ plugins/unveilhooks/ls.unveilhooks.js
@@ -55,8 +55,8 @@ For background images, use data-bg attribute:
 			// fix toDataURL SecurityError: The operation is insecure
 			img.crossOrigin = 'anonymous';
 			img.onload = function() {
-				if (isiOS && lazySizes.cfg.useDataURL) {
-					// avoid flicker when background-image change
+				if (lazySizes.cfg.useDataURL) {
+					// avoid flicker when background-image change while cache is disabled
 					//Set canvas size is same as the picture
 					canvas.width = img.width;
 					canvas.height = img.height;
